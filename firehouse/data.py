1c740d88e4a5a8dd28f48b4a754c4a5ae09ef512
Brennan Vincello <brennanv03@gmail.com>
2017-09-20 15:00:12 -0700

    add parcel data

--- firehouse/data.py
+++ firehouse/data.py
@@ -1,12 +1,18 @@
 from .weather import get_weather
+from .parcel import get_parcel
 import json
 
 with open('demo/data/F01705150050.json') as f:
     data = json.load(f)
 
+# Enrich data with weather data 
 event_opened = data['description']['event_opened']
 date, time = event_opened.split('T')
 year, month, day = date.split('-')
 hour = time.split(':')[0]
 zipcode = '23218'  # TODO geocode address
 data['weather'] = get_weather(zipcode, year, month, day)
+
+# Enrich data with parcel data
+lat, lon = data['address']['latitude'], data['address']['longitude']
+data['parcel'] = get_parcel(lat, lon)
---
+++ firehouse/parcel.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def get_parcel(lat, lon):
+    url = ('http://gis.richmondgov.com/ArcGIS/rest/services/WebMercator/Parcels/MapServer/2/query'
+           '?geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&returnCountOnly=false'
+           '&returnIdsOnly=false&returnGeometry=true&f=pjson')
+    url += f'&geometry={lon}%2C{lat}'
+    response = requests.get(url)
+    return response.json()
