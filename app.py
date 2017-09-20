ace9c2a3bad63e82d5c1c6f985ad67bd1bfba138
Brennan Vincello <brennanv03@gmail.com>
2017-09-20 14:07:47 -0700

    add weather

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, render_template
 from firehouse.data import data
 from firehouse.map import create_map
+from firehouse.weather import get_weather
 
 app = Flask(__name__)
 
 
 
-@app.route('/data/raw')
-def raw():
+@app.route('/data')
+def data():
     return jsonify(data)
 
 
@@ -16,6 +17,12 @@ def raw():
 def map():
     _map = create_map(data)
     return render_template('map.html', map=_map)
+
+
+@app.route('/weather/<zipcode>/<year>/<month>/<day>')
+def weather():
+    hourly_weather = get_weather(zipcode, year, month, day)
+    return jsonify(hourly_weather)
 
 
 if __name__ == '__main__':
