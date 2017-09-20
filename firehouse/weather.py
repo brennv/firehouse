ea5cfa695ffdf080d26052de77137e99661e7286
Brennan Vincello <brennanv03@gmail.com>
2017-09-20 15:00:43 -0700

    clean up url

--- firehouse/weather.py
+++ firehouse/weather.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 def get_weather(zipcode, year, month, day, hour=None):
     """ Retrieve hourly weather data for a specified date.
     If given an hour, only return data for that hour. """
-    url = f'https://www.wunderground.com/history/airport/KRIC/{year}/{month}/{day}'
-    url += f'/DailyHistory.html?reqdb.zip={zipcode}&reqdb.magic=1&reqdb.wmo=99999&MR=1'
+    url = (f'https://www.wunderground.com/history/airport/KRIC/{year}/{month}/{day}'
+           f'/DailyHistory.html?reqdb.zip={zipcode}&reqdb.magic=1&reqdb.wmo=99999&MR=1')
     response = requests.get(url)
-    # TODO add status code handler
+    # TODO add error handler
     soup = BeautifulSoup(response.text, 'html5lib')
     table = soup.find_all('table')[-1]
     df = pd.read_html(str(table))[0]
