475a380654cf7835bdc9d3b3523242fbd1d4790a
Brennan Vincello <brennanv03@gmail.com>
2017-09-20 14:08:22 -0700

    enrich data
    with weather data

--- firehouse/data.py
+++ firehouse/data.py
@@ -1,5 +1,12 @@
+from .weather import get_weather
 import json
-
 
 with open('demo/data/F01705150050.json') as f:
     data = json.load(f)
+
+event_opened = data['description']['event_opened']
+date, time = event_opened.split('T')
+year, month, day = date.split('-')
+hour = time.split(':')[0]
+zipcode = '23218'  # TODO geocode address
+data['weather'] = get_weather(zipcode, year, month, day)
--- firehouse/weather.py
+++ firehouse/weather.py
@@ -1,23 +1,19 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-import html5lib
-from io import StringIO
 
 
-year, month, day = 2013, 9, 20
-url = f'https://www.wunderground.com/history/airport/KRIC/{year}/{month}/{day}/DailyHistory.html?req_city=Richmond&req_state=VA&req_statename=Virginia&reqdb.zip=23218&reqdb.magic=1&reqdb.wmo=99999&MR=1'
-print(url)
-
-response = requests.get(url)
-print(response)
-
-soup = BeautifulSoup(response.text, 'html5lib')
-table = soup.find_all('table')[-1]
-
-with open('env/temp.txt') as f:
-    f.write(table)
-
-df = pd.read_html(StringIO(table))
-
-print(df)
+def get_weather(zipcode, year, month, day, hour=None):
+    """ Retrieve hourly weather data for a specified date.
+    If given an hour, only return data for that hour. """
+    url = f'https://www.wunderground.com/history/airport/KRIC/{year}/{month}/{day}'
+    url += f'/DailyHistory.html?reqdb.zip={zipcode}&reqdb.magic=1&reqdb.wmo=99999&MR=1'
+    response = requests.get(url)
+    # TODO add status code handler
+    soup = BeautifulSoup(response.text, 'html5lib')
+    table = soup.find_all('table')[-1]
+    df = pd.read_html(str(table))[0]
+    weather = df.to_dict(orient='records')
+    if hour != None:
+        weather = weather[hour]
+    return weather
