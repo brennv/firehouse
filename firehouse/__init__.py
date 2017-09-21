02e1f5a31f0f57bac6ea48a95c3a644048a64d15
Brennan Vincello <brennanv03@gmail.com>
2017-09-21 08:52:47 -0700

    fix linter warnings

--- firehouse/__init__.py
+++ firehouse/__init__.py
@@ -3,3 +3,12 @@ from .map import create_map
 from .weather import get_weather
 from .parcel import get_parcel
 from .district import get_district
+
+
+__all__ = [
+    'enrich_data',
+    'create_map',
+    'get_weather',
+    'get_parcel',
+    'get_district',
+    ]
