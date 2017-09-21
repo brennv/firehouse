ffe3b8c9e0c0982f97ffbfbb8cafdf32f9e47c0d
Brennan Vincello <brennanv03@gmail.com>
2017-09-21 12:18:19 -0700

    unlocalize district_names

--- firehouse/district.py
+++ firehouse/district.py
@@ -7,6 +7,8 @@ with open('demo/Fire_Districts.geojson') as f:
     # data.richmondgov.com/Community-Safety-and-Well-Being/Fire-Districts/f9rn-dwnd
     mp = geojson.load(f)
 
+district_names = [s['properties']['name'] for s in mp['features']]
+
 
 def get_district(lat, lon):
     """ Check if polygons in a multiploygon contain a point.
@@ -14,5 +16,4 @@ def get_district(lat, lon):
     point = Point(float(lon), float(lat))
     point_in_poly = [shape(s['geometry']).contains(point) for s in mp['features']]
     index = point_in_poly.index(True)
-    district_names = [s['properties']['name'] for s in mp['features']]
     return district_names[index]
