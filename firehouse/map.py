1a4a3a1d770794df48bf7b4f8c6a0f15cde680e5
Brennan Vincello <brennanv03@gmail.com>
2017-09-20 18:37:33 -0700

    refactor popups

--- firehouse/map.py
+++ firehouse/map.py
@@ -3,10 +3,33 @@ import folium
 richmond_coords = [37.53, -77.46]
 
 
+def create_popup(data):
+    address_line1 = data['address']['address_line1']
+    event_opened = data['description']['event_opened']
+    date, time = event_opened.split('T')
+    year, month, day = date.split('-')
+    hour, minute = time.split(':')[:2]
+    _type = data['description']['type']
+    subtype = data['description']['subtype']
+    weather = data['weather'][int(hour)]['Conditions']
+    parcel = data['parcel']['spatialReference']['wkid']
+    stations = [x['station'] for x in data['apparatus']]
+    stations = ', '.join(list(set(stations)))
+    popup = f"""
+        <code>{_type}: {subtype}</code>
+        <h4>{address_line1}</h4>
+        <h5>{date} {hour}:{minute}<h5>
+        <p>Weather: {weather}</p>
+        <p>Parcel: {parcel}</p>
+        <p>Stations involved: {stations}</p>
+        """
+    return popup
+
+
 def create_map(data):
     """ Generate a LeafletJS div section using Python Folium. """
     m = folium.Map(location=richmond_coords)
     lat, lon = data['address']['latitude'], data['address']['longitude']
-    address_line1 = data['address']['address_line1']
-    folium.Marker([lat, lon], popup=address_line1).add_to(m)
+    popup = create_popup(data)
+    folium.Marker([lat, lon], popup=popup).add_to(m)
     return m._repr_html_()
